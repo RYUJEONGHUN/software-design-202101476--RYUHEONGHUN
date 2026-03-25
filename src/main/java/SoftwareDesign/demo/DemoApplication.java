4de2e5ca215f38674a1b3b78e880768e71188880
RYUJEONGHUN <ryugh0210@naver.com>
2026-03-25 16:34:39 +0900

    first commit

---
+++ src/main/java/SoftwareDesign/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package SoftwareDesign.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
