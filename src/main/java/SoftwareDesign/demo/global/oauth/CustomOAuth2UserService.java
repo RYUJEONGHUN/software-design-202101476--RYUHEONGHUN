bfb2faff843d10d4ffd75619c487ebb663ca6e65
RYUJEONGHUN <ryugh0210@naver.com>
2026-03-27 22:13:16 +0900

    구글 로그인 후 유저정보 받아와서 db연결

---
+++ src/main/java/SoftwareDesign/demo/global/oauth/CustomOAuth2UserService.java
@@ -0,0 +1,51 @@
+package SoftwareDesign.demo.global.oauth;
+
+import SoftwareDesign.demo.domain.user.entity.User;
+import SoftwareDesign.demo.domain.user.entity.UserRole;
+import SoftwareDesign.demo.domain.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // google
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        String email = (String) attributes.get("email");
+        String name = (String) attributes.get("name");
+
+        User user = userRepository.findByUsername(email)
+                .map(entity -> entity.update(name))
+                .orElse(User.builder()
+                        .username(email)
+                        .name(name)
+                        .role(UserRole.GUEST) // 최초 가입 시 게스트 권한
+                        .build());
+
+        userRepository.save(user);
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRole().name())),
+                attributes,
+                userNameAttributeName);
+    }
+}
