368f91b7f8a4850e0d2aa94eb578ae8dccbdfde9
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-07 00:29:37 +0900

    과목 관련

---
+++ src/main/java/SoftwareDesign/demo/domain/subject/entity/Subject.java
@@ -0,0 +1,29 @@
+package SoftwareDesign.demo.domain.subject.entity;
+
+import SoftwareDesign.demo.domain.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="subjects")
+public class Subject extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Builder
+    public Subject(String name){
+        this.name=name;
+    }
+
+
+}
