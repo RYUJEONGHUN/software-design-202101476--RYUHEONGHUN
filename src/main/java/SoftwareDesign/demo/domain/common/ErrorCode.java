50ee12ab646ac303bf02bc9e6686b6b621080a49
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-08 15:34:39 +0900

    에러코드 추가

--- src/main/java/SoftwareDesign/demo/domain/common/ErrorCode.java
+++ src/main/java/SoftwareDesign/demo/domain/common/ErrorCode.java
@@ -13,10 +13,13 @@ public enum ErrorCode {
     STUDENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 학생을 찾을 수 없네."),
     SUBJECT_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 과목을 찾을 수 없네."),
     ALREADY_GRADE_EXIST(HttpStatus.BAD_REQUEST, "이미 이 학기에 해당 과목 성적이 등록되어 있구먼!"),
+    ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST, "해당 유저가 이미 있어"),
+    ALREADY_REGISTERED_STUDENT(HttpStatus.BAD_REQUEST, "이미 해당 학생이 있어"),
+    ALREADY_REGISTERED_TEACHER(HttpStatus.BAD_REQUEST, "이미 해당 선생이 있어"),
     INVALID_SCORE(HttpStatus.BAD_REQUEST, "성적은 0점에서 100점 사이여야 하네."),
 
     // 401 Unauthorized
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요한 서비스라네."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요한 서비스."),
 
     // 403 Forbidden
     FORBIDDEN(HttpStatus.FORBIDDEN, "자네는 권한이 없구먼!"),
@@ -27,7 +30,7 @@ public enum ErrorCode {
     DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 데이터를 찾을 수 없구먼."),
 
     // 500 Internal Server Error
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 삼촌이 좀 아픈가 보네...");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버가 좀 아픈가 보네...");
 
     private final HttpStatus httpStatus;
     private final String message;
