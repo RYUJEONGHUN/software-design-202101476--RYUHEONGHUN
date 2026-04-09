430d716034dabd56a4c73f400571a2ec92e3fd2a
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-09 18:28:30 +0900

    짜잘한것들

--- src/main/java/SoftwareDesign/demo/domain/common/ErrorCode.java
+++ src/main/java/SoftwareDesign/demo/domain/common/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
     ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST, "해당 유저가 이미 있어"),
     ALREADY_REGISTERED_STUDENT(HttpStatus.BAD_REQUEST, "이미 해당 학생이 있어"),
     ALREADY_REGISTERED_TEACHER(HttpStatus.BAD_REQUEST, "이미 해당 선생이 있어"),
+    ALREADY_ATTENDANCE_CHECKED(HttpStatus.BAD_REQUEST, "이미 출석체크 처리되어있어"),
     INVALID_SCORE(HttpStatus.BAD_REQUEST, "성적은 0점에서 100점 사이여야 하네."),
 
     // 401 Unauthorized
@@ -25,10 +26,9 @@ public enum ErrorCode {
     FORBIDDEN(HttpStatus.FORBIDDEN, "자네는 권한이 없구먼!"),
     NOT_TEACHER(HttpStatus.FORBIDDEN, "자네는 선생님이 아니라서 성적을 넣을 수 없네!"),
     NOT_STUDENT(HttpStatus.FORBIDDEN, "자네는 학생이 아니구먼!"),
-
     // 404 Not Found
     DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 데이터를 찾을 수 없구먼."),
-
+    ATTENDANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "출석정보를 찾을 수 없다"),
     // 500 Internal Server Error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버가 좀 아픈가 보네...");
 
