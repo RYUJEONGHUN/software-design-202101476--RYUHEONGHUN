6f384872e59bc1ae11ca4bb9733b25fced175a9d
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-14 16:51:05 +0900

    초기 데이터 설정

--- src/main/java/SoftwareDesign/demo/domain/common/DataInitializer.java
+++ src/main/java/SoftwareDesign/demo/domain/common/DataInitializer.java
@@ -14,10 +14,18 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         if (subjectRepository.count() == 0) {
-            subjectRepository.save(new Subject("국어"));
-            subjectRepository.save(new Subject("영어"));
-            subjectRepository.save(new Subject("수학"));
-            subjectRepository.save(new Subject("알고리즘"));
+            subjectRepository.save(new Subject("국어",1));
+            subjectRepository.save(new Subject("영어",1));
+            subjectRepository.save(new Subject("수학",1));
+            subjectRepository.save(new Subject("알고리즘",1));
+            subjectRepository.save(new Subject("데이터프로그래밍",2));
+            subjectRepository.save(new Subject("자료구조",2));
+            subjectRepository.save(new Subject("컴퓨터구조",2));
+            subjectRepository.save(new Subject("대학수학",2));
+            subjectRepository.save(new Subject("네트워크보안",3));
+            subjectRepository.save(new Subject("웹프로그래밍",3));
+            subjectRepository.save(new Subject("운영체제",3));
+            subjectRepository.save(new Subject("리눅스",3));
             System.out.println("초기 과목 데이터가 생성");
         }
     }
