f964a227c3659b4c3ab9dbe7e3f14c6482071aed
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-14 16:50:15 +0900

    에러코드 추가

--- src/main/java/SoftwareDesign/demo/domain/common/ErrorCode.java
+++ src/main/java/SoftwareDesign/demo/domain/common/ErrorCode.java
@@ -12,11 +12,13 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 유저를 찾을 수 없네."),
     STUDENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 학생을 찾을 수 없네."),
     TEACHER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 선생을 찾을 수 없네."),
+    PARENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 학부모를 찾을 수 없네."),
     SUBJECT_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 과목을 찾을 수 없네."),
     ALREADY_GRADE_EXIST(HttpStatus.BAD_REQUEST, "이미 이 학기에 해당 과목 성적이 등록되어 있구먼!"),
     ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST, "해당 유저가 이미 있어"),
     ALREADY_REGISTERED_STUDENT(HttpStatus.BAD_REQUEST, "이미 해당 학생이 있어"),
     ALREADY_REGISTERED_TEACHER(HttpStatus.BAD_REQUEST, "이미 해당 선생이 있어"),
+    ALREADY_REGISTERED_PARENT(HttpStatus.BAD_REQUEST, "이미 해당 학부모가 있어"),
     ALREADY_ATTENDANCE_CHECKED(HttpStatus.BAD_REQUEST, "이미 출석체크 처리되어있어"),
     INVALID_SCORE(HttpStatus.BAD_REQUEST, "성적은 0점에서 100점 사이여야 하네."),
 
