4078b6319d8ae46ca6387092b2ccee8ace6c2353
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-15 23:04:51 +0900

    알람 엔티티 & 타입

---
+++ src/main/java/SoftwareDesign/demo/domain/notification/entity/NotificationType.java
@@ -0,0 +1,8 @@
+package SoftwareDesign.demo.domain.notification.entity;
+
+public enum NotificationType {
+    GRADE_UPDATED,    // 성적 입력됨
+    ATTENDANCE_UPDATED, //
+    FEEDBACK_CREATED, // 피드백 작성됨
+    CONSULTATION_UPDATED // 상담 내역 업데이트됨
+}
