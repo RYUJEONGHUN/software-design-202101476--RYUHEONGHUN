895885770d343bff73ee8dbbba057e04dfbd5427
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-15 23:09:04 +0900

    parent

--- src/main/java/SoftwareDesign/demo/domain/parent/repository/ParentRepository.java
+++ src/main/java/SoftwareDesign/demo/domain/parent/repository/ParentRepository.java
@@ -4,9 +4,11 @@ import SoftwareDesign.demo.domain.parent.entity.Parent;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ParentRepository extends JpaRepository<Parent, Long> {
     @Query("select p from Parent p " +
             "join fetch p.children pc " +
@@ -14,4 +16,9 @@ public interface ParentRepository extends JpaRepository<Parent, Long> {
             "join fetch s.user " +
             "where p.id = :parentId")
     Optional<Parent> findByIdWithChildren(@Param("parentId") Long parentId);
+
+    @Query("select p from Parent p " +
+            "join p.children pc " +
+            "where pc.student.id = :studentId")
+    Optional<Parent> findByStudentId(@Param("studentId") Long studentId);
 }
