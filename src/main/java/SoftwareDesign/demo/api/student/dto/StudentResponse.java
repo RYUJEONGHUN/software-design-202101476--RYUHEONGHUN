40d0c841b941b99c0cae52c48fc6ac00a1cfe56c
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-20 17:59:02 +0900

    id 반환

--- src/main/java/SoftwareDesign/demo/api/student/dto/StudentResponse.java
+++ src/main/java/SoftwareDesign/demo/api/student/dto/StudentResponse.java
@@ -5,9 +5,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+// 출석,상담 관련
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudentResponse {
+    private Long id;
     private String name;
     private String studentNumber;
     private int grade;
@@ -15,6 +17,7 @@ public class StudentResponse {
     private int number;
 
     public StudentResponse(Student student) {
+        this.id = student.getId();
         this.name = student.getUser().getName();
         this.studentNumber = student.getStudentNumber();
         this.grade = student.getGrade();
