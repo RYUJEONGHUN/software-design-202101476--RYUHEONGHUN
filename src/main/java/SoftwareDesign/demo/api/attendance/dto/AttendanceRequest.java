7909c8fa2f14ecefa92686bcf6f49d83ede0597d
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-20 17:58:13 +0900

    출석 생성 --> 날짜 추가

--- src/main/java/SoftwareDesign/demo/api/attendance/dto/AttendanceRequest.java
+++ src/main/java/SoftwareDesign/demo/api/attendance/dto/AttendanceRequest.java
@@ -4,6 +4,9 @@ import SoftwareDesign.demo.domain.attendance.entity.AttendanceStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
@@ -15,4 +18,7 @@ public class AttendanceRequest {
     )
     private AttendanceStatus status; // 출석/지각/결석/공결 상태
     private String note;         // 특이사항
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate date;
 }
