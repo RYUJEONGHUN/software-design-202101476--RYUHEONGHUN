94a4ad444ea8c53471576a3335e79ffffb8ea7df
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-20 13:50:45 +0900

    학생 관련

--- src/main/java/SoftwareDesign/demo/api/student/dto/StudentResponse.java
+++ src/main/java/SoftwareDesign/demo/api/student/dto/StudentResponse.java
@@ -1,18 +1,21 @@
 package SoftwareDesign.demo.api.student.dto;
 
 import SoftwareDesign.demo.domain.student.entity.Student;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudentResponse {
-    private final String name;
-    private final String studentNumber;
-    private final int grade;
-    private final int classNum;
-    private final int number;
+    private String name;
+    private String studentNumber;
+    private int grade;
+    private int classNum;
+    private int number;
 
     public StudentResponse(Student student) {
-        this.name = student.getUser().getName(); // User 엔티티에서 이름을 가져옴
+        this.name = student.getUser().getName();
         this.studentNumber = student.getStudentNumber();
         this.grade = student.getGrade();
         this.classNum = student.getClassNum();
