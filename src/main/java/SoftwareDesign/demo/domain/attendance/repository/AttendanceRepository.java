524e5999f5f1e78936d67b035753a14bd1b172d7
RYUJEONGHUN <ryugh0210@naver.com>
2026-04-20 18:00:06 +0900

    출석 관련 코드들

--- src/main/java/SoftwareDesign/demo/domain/attendance/repository/AttendanceRepository.java
+++ src/main/java/SoftwareDesign/demo/domain/attendance/repository/AttendanceRepository.java
@@ -19,11 +19,10 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     // 특정 학생의 특정 기간(한 달 등) 출석 기록 조회
     List<Attendance> findAllByStudentIdAndDateBetween(Long studentId, LocalDate startDate, LocalDate endDate);
 
-    // 특정 날짜의 전교생 출석 현황 조회
-    List<Attendance> findAllByDate(LocalDate date);
+    @Query("SELECT s FROM Student s WHERE NOT EXISTS " +
+            "(SELECT 1 FROM Attendance a WHERE a.student = s AND a.date = :date)")
+    List<Student> findUnmarkedStudentsByDate(@Param("date") LocalDate date);
 
-    // 특정 학생의 모든 출석 기록
-    List<Attendance> findAllByStudentId(Long studentId);
 
     @Query("SELECT new SoftwareDesign.demo.api.attendance.dto.AttendanceCount(" +
             "COALESCE(SUM(CASE WHEN a.status = 'PRESENT' THEN 1L ELSE 0L END), 0L), " +
@@ -32,6 +31,17 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
             "COALESCE(SUM(CASE WHEN a.status = 'EXCUSED' THEN 1L ELSE 0L END), 0L)) " +
             "FROM Attendance a WHERE a.student.id = :studentId")
     AttendanceCount getTotalCounts(@Param("studentId") Long studentId);
+
+    @Query("SELECT new SoftwareDesign.demo.api.attendance.dto.AttendanceCount(" +
+            "COALESCE(SUM(CASE WHEN a.status = 'PRESENT' THEN 1L ELSE 0L END), 0L), " +
+            "COALESCE(SUM(CASE WHEN a.status = 'ABSENT' THEN 1L ELSE 0L END), 0L), " +
+            "COALESCE(SUM(CASE WHEN a.status = 'TARDY' THEN 1L ELSE 0L END), 0L), " +
+            "COALESCE(SUM(CASE WHEN a.status = 'EXCUSED' THEN 1L ELSE 0L END), 0L))" +
+            "FROM Attendance a WHERE a.student.id = :studentId " +
+                    "AND a.date BETWEEN :start AND :end")
+    AttendanceCount getMonthlyCounts(@Param("studentId") Long studentId,
+                                     @Param("start") LocalDate start,
+                                     @Param("end") LocalDate end);
 
 
     Boolean existsByStudentAndDate(Student student,LocalDate today);
